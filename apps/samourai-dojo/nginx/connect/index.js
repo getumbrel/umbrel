ddfa72bd8e6108c9a6347afcf5455cc0dff1ae05
Lounès Ksouri <dev@louneskmt.com>
2021-02-03 22:57:54 +0100

    Add connect static page

---
+++ apps/samourai-dojo/nginx/connect/index.js
@@ -0,0 +1,30 @@
+var baseRoute = "";
+
+if(bitcoinNetwork == "testnet") {
+  baseRoute = "v2";
+} else {
+  baseRoute = "test/v2"
+}
+
+var pairingRequest = `http://node:8080/${baseRoute}/${supportPrefix}/pairing`
+
+// TESTNET: GET /test/v2/support/pairing
+// MAINNET: GET /v2/support/pairing
+
+/* Returns
+
+{
+  "pairing": {
+    "type": "dojo.api",
+    "version": "1.8.0",
+    "apikey": "00000000000000000000000000000000",
+  }
+}
+
+*/
+
+var pairingInfo = "" // Request response
+
+pairingInfo.pairing.url = `http://${dojoHiddenService}/${baseRoute}`
+
+// Encode pairingInfo in a QR code to be scanned by Dojo
