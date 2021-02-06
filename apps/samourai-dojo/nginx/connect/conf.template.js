f7ecedc00abd18d0a7b69d19a6e5dde0c0256ef6
Mayank <mayankchhabra9@gmail.com>
2021-02-06 13:06:53 +0530

    Add Samourai Server web UI

--- apps/samourai-dojo/nginx/connect/conf.template.js
+++
@@ -1,4 +0,0 @@
-var dojoHiddenService = "$DOJO_HIDDEN_SERVICE"
-var whirlpoolHiddenService = "$WHIRLPOOL_HIDDEN_SERVICE"
-var bitcoinNetwork = "$COMMON_BTC_NETWORK"
-var supportPrefix = "$NODE_PREFIX_SUPPORT"
---
+++ apps/samourai-dojo/nginx/connect/js/conf.template.js
@@ -0,0 +1,4 @@
+var dojoHiddenService = "$DOJO_HIDDEN_SERVICE";
+var whirlpoolHiddenService = "$WHIRLPOOL_HIDDEN_SERVICE";
+var bitcoinNetwork = "$COMMON_BTC_NETWORK";
+var supportPrefix = "$NODE_PREFIX_SUPPORT";
