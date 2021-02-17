257cce578a2844de0e533f53bfe07149c1a853e4
Luke Childs <lukechilds123@gmail.com>
2021-02-17 20:05:13 +0700

    Link to Dojo Maintnenace Tools from Samourai Server app

--- apps/samourai-server/nginx/connect/js/conf.template.js
+++ apps/samourai-server/nginx/connect/js/conf.template.js
@@ -1,5 +1,5 @@
 var dojoHiddenService = "$DOJO_HIDDEN_SERVICE";
 var whirlpoolHiddenService = "http://$WHIRLPOOL_HIDDEN_SERVICE";
 var bitcoinNetwork = "$COMMON_BTC_NETWORK";
-var apiKey = "$NODE_ADMIN_KEY";
+var dojoAdminKey = "$NODE_ADMIN_KEY";
 var supportPrefix = "$NODE_PREFIX_SUPPORT";
