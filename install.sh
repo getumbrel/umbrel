f57b64d00800b68fa4c7af717862753c0282052a
Aaron Dewes <aaron.dewes@web.de>
2021-03-07 20:13:06 +0100

    Text improvments

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ fi
 if ! command -v pip3 >/dev/null 2>&1; then
     echo "pip3 is not installing."
     echo "Plase install it to continue installing Umbrel."
-    echo "On Debian-based distros, use 'sudo apt-get install python3-pip' to install it"
+    echo "On Debian-based distros, use 'sudo apt-get install python3-pip' to install it."
 fi
 
 if ! command -v docker >/dev/null 2>&1; then
@@ -48,12 +48,12 @@ if ! command -v docker-compose >/dev/null 2>&1; then
     pip3 install docker-compose
 fi
 
-echo "Where should we install Umbrel? Please provide the full path where umbrel should be stored."
+echo "Where should we install Umbrel? Please provide the full path where Umbrel should be stored."
 echo "If none is entered, we'll use /etc/umbrel."
 read install_dir
 
 echo "Do you want us to use an external drive for Umbrel? If yes, please provide the path of that device in /dev"
-echo "Please make sure to NOT enter the drive your OS is installed on, all data on the drive will be deleted"
+echo "All data on that drive will be erased, so make sure not to select one with data you still need."
 echo "If you want to use the same disk for Umbrel and your OS, select none."
 select opt in $(list_block_devices_without_root) none
 do
@@ -106,7 +106,7 @@ else
     echo "Installing Umbrel (${btc_network}) in $install_dir."
 fi
 
-echo "Press enter to start the setup"
+echo "Press enter to start the setup".
 read
 
 echo "Starting setup..."
@@ -115,21 +115,21 @@ cd ${install_dir}
 echo "Downloading Umbrel..."
 curl -L https://github.com/getumbrel/umbrel/archive/v${UMBREL_VERSION}.tar.gz | tar -xz --strip-components=1
 
-echo "Configuring Umbrel"
+echo "Configuring Umbrel..."
 NETWORK=$btc_network ./scripts/configure
 
 if [[ ! -z "${external_disk}" ]]; then
-    echo "Installing mount service"
+    echo "Installing mount service..."
     install -m 644 ./scripts/services/umbrel-external-storage.service /etc/systemd/system/umbrel-external-storage.service
     sed -i -e "s/\/home\/umbrel\/umbrel/${install_dir}/g" /etc/systemd/system/umbrel-external-storage.service
     sed -i -e "s/EXTERNAL_STORAGE_PATH=\"undefined\"/EXTERNAL_STORAGE_PATH=\"${external_disk}\"/g" /etc/systemd/system/umbrel-external-storage.service
 fi
 
-echo "Installing startup service"
+echo "Installing startup service..."
 install -m 644 ./scripts/services/umbrel-startup.service /etc/systemd/system/umbrel-startup.service
 sed -i -e "s/\/home\/umbrel\/umbrel/${install_dir}/g" /etc/systemd/system/umbrel-startup.service
 
-echo "Starting services"
+echo "Starting services..."
 systemctl enable umbrel-startup
 if [[ ! -z "${external_disk}" ]]; then
     systemctl enable umbrel-external-storage
