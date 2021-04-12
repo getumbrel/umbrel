09ecc8b18b48ce2fe95b6fad2ba20b83d866abaa
Luke Childs <lukechilds123@gmail.com>
2021-04-12 19:04:12 +0700

    Loop over glob not ls output

--- scripts/update/01-run.sh
+++ scripts/update/01-run.sh
@@ -62,7 +62,7 @@ if [[ ! -z "${UMBREL_OS:-}" ]]; then
     fi
 
     # This makes sure systemd services are always updated (and new ones are enabled).
-    UMBREL_SYSTEMD_SERVICES=$(ls ${UMBREL_ROOT}/.umbrel-${RELEASE}/scripts/umbrel-os/services/*.service)
+    UMBREL_SYSTEMD_SERVICES="${UMBREL_ROOT}/.umbrel-${RELEASE}/scripts/umbrel-os/services/*.service"
     for service in $UMBREL_SYSTEMD_SERVICES; do
       install -m 644 "${service}" "/etc/systemd/system/${service}"
       systemctl enable "${service}"
