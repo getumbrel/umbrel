6e86f19963540e65028433c4bc085c3d7aed42e4
Luke Childs <lukechilds123@gmail.com>
2021-06-28 01:15:21 +0700

    Fix path

--- os/build.sh
+++ os/build.sh
@@ -1,6 +1,8 @@
+# SCRIPT_DIR="$(readlink $(dirname "${BASH_SOURCE[0]}"))"
+SCRIPT_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 docker run \
   --rm \
   --privileged \
   -v /dev:/dev \
-  -v ${PWD}:/build \
+  -v ${SCRIPT_DIR}:/build \
   mkaczanowski/packer-builder-arm build raspberrypi.json
