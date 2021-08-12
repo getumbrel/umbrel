2c842f53ed23c636520808475650b89aa6aa8064
Lounès Ksouri <dev@louneskmt.com>
2021-08-12 15:06:21 +0200

    Update script to handle Dojo variable hostname

--- apps/samourai-server/nginx/connect/js/script.js
+++ apps/samourai-server/nginx/connect/js/script.js
@@ -1,9 +1,16 @@
+let dojoHost = `${window.location.hostname}:${dojoLocalPort}`;
+let dojoBaseRoute = bitcoinNetwork == "testnet" ? "test/v2" : "v2";
+
+if(window.location.hostname.endsWith(".onion")) {
+  dojoHost = dojoHiddenService;
+}
+
 document.getElementById('dojo-admin-key').innerText = dojoAdminKey;
 document.getElementById('whirlpool-api-key').innerText = whirlpoolApiKey;
+document.getElementById('whirlpool-hidden-service').innerText = `http://${whirlpoolHiddenService}`;
+document.getElementById('dmt-link').setAttribute("href", `http://${dojoHost}/admin`);
 
-var baseRoute = bitcoinNetwork == "testnet" ? "test/v2" : "v2";
-
-fetch(`http://${window.location.host}/${baseRoute}/auth/login`, {
+fetch(`http://${dojoHost}/${dojoBaseRoute}/auth/login`, {
     method: 'POST',
     headers: new Headers({
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -12,7 +19,7 @@ fetch(`http://${window.location.host}/${baseRoute}/auth/login`, {
  })
  .then(response => response.json())
  .then(data => {
-    fetch(`http://${window.location.host}/${baseRoute}/${supportPrefix}/pairing`, {
+    fetch(`http://${dojoHost}/${dojoBaseRoute}/${dojoSupportPrefix}/pairing`, {
         method: 'GET',
         headers: new Headers({
             'Authorization': 'Bearer ' + data.authorizations.access_token,
@@ -21,8 +28,8 @@ fetch(`http://${window.location.host}/${baseRoute}/auth/login`, {
     })
     .then(response => response.json())
     .then(data => {
-        var pairingInfo = data;
-        pairingInfo.pairing.url = `http://${dojoHiddenService}/${baseRoute}`;
+        let pairingInfo = data;
+        pairingInfo.pairing.url = `http://${dojoHiddenService}/${dojoBaseRoute}`;
 
         const qrcodeSvg = new QRCode({
           content: JSON.stringify(pairingInfo),
@@ -34,15 +41,5 @@ fetch(`http://${window.location.host}/${baseRoute}/auth/login`, {
           ecl: "M",
         }).svg();
         document.querySelector('.qr-contents').innerHTML = qrcodeSvg;
-
-        document.getElementById('whirlpool-hidden-service').innerText = `${whirlpoolHiddenService}`;
     });
  });
-
-let dojoHost = `${window.location.host}:${dojoLocalPort}`;
-
-if(window.location.host.endsWith(".onion")) {
-  dojoHost = dojoHiddenService;
-}
-
-document.getElementById('dmt-link').setAttribute("href", `http://${dojoHost}/admin`);
