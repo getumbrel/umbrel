5bcc5c9610090c9972e9c86b94d274147d94b2ae
Charles Hill <chill@degreesofzero.com>
2022-01-21 15:42:32 +0000

    Remove extra entrypoint.sh, provide app password via env

--- apps/bleskomat-server/data/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export BLESKOMAT_SERVER_ADMIN_PASSWORD=$(npm run --silent generate:adminPasswordHash -- "$APP_PASSWORD" | xargs)
-
-exec "/usr/src/app/docker-entrypoint.sh $@"
