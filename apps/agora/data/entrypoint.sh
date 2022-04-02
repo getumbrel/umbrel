f1a268cb31d2cc4955e67e1c3cbb8b296637d4f8
yzernik <yzernik@gmail.com>
2022-04-02 04:27:42 -0700

    Cleanup and remove old files

--- apps/agora/data/entrypoint.sh
+++ apps/agora/data/entrypoint.sh
@@ -3,8 +3,6 @@
 # Update configs
 /filebrowser config init
 /filebrowser config set --branding.name "Agora Admin"
-# /filebrowser config set --port 8080
-# /filebrowser config set --baseurl "/admin/"
 /filebrowser users add umbrel ${APP_PASSWORD}
 
 exec /filebrowser -p 8080 --baseurl "/admin/files"
