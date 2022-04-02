111a69f92e94318e01989659c0bfb5ad3bb311bb
yzernik <yzernik@gmail.com>
2022-04-02 04:11:26 -0700

    Use nginx reverse proxy to serve both agora and filebrowser, as well as a static landing page.

--- apps/agora/data/entrypoint.sh
+++ apps/agora/data/entrypoint.sh
@@ -3,6 +3,8 @@
 # Update configs
 /filebrowser config init
 /filebrowser config set --branding.name "Agora Admin"
+# /filebrowser config set --port 8080
+# /filebrowser config set --baseurl "/admin/"
 /filebrowser users add umbrel ${APP_PASSWORD}
 
-exec /filebrowser
+exec /filebrowser -p 8080 --baseurl "/admin/files"
