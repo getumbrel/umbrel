eb15e75cb28e37367698427cd711d4d387022230
yzernik <yzernik@gmail.com>
2022-04-09 10:36:08 -0700

    Change filebrowser custom branding name

--- apps/agora/data/entrypoint.sh
+++ apps/agora/data/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Update configs
 /filebrowser config init
-/filebrowser config set --branding.name "Agora Admin"
+/filebrowser config set --branding.name "Agora Admin File Browser"
 /filebrowser users add umbrel ${APP_PASSWORD}
 
 exec /filebrowser -p 8080 --baseurl "/admin/files"
