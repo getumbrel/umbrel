072d4b0ff66ca26276ef0f3d18d5b8aea59b35f3
Steven Briscoe <stevebriscoe123@gmail.com>
2022-05-07 18:48:14 +0200

    App Repo Extras

--- scripts/update/steps/migrate-to-repo.sh
+++ scripts/update/steps/migrate-to-repo.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 RELEASE=$1
 UMBREL_ROOT=$2
 
-DEFAULT_UMBREL_APP_REPO_URL="git@github.com:nevets963/umbrel-repo.git"
-
 APP_REPOS_MIGRATION_DONE_FILE="${UMBREL_ROOT}/db/.app-repos-migration"
 
 if [[ ! -f "${APP_REPOS_MIGRATION_DONE_FILE}" ]]; then
@@ -15,7 +13,8 @@ if [[ ! -f "${APP_REPOS_MIGRATION_DONE_FILE}" ]]; then
 {"state": "installing", "progress": 70, "description": "Reconfiguring apps", "updateTo": "$RELEASE"}
 EOF
   
-  # 1. Install 'gettext-base' if not installed (needed for envsubst)
+  # 1. Install 'gettext-base' if not installed
+  # This needed for 'envsubst' which is used for templating
   REQUIRED_PKG="gettext-base"
   PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG | grep "install ok installed")
   if [[ "" = "${PKG_OK}" ]]; then
@@ -23,27 +22,36 @@ EOF
   fi
 
   # 2. Set the default remote app remote (Add 'appRepo' to db/user.json)
+  DEFAULT_UMBREL_APP_REPO_URL=$("${UMBREL_ROOT}/scripts/repo" default-repo)
   echo "Adding default Umbrel app repo: ${DEFAULT_UMBREL_APP_REPO_URL}"
-  $UMBREL_ROOT/scripts/repo set "${DEFAULT_UMBREL_APP_REPO_URL}"
+  "${UMBREL_ROOT}/scripts/repo" set "${DEFAULT_UMBREL_APP_REPO_URL}"
 
   # 3. Clone remote repo locally
-  $UMBREL_ROOT/scripts/repo update
+  "${UMBREL_ROOT}/scripts/repo" update || true
 
-  # 4. Update all apps using the app repo as source of truth
-  for app in $("${UMBREL_ROOT}/scripts/app" ls-installed); do
-    if [[ "${app}" != "" ]]; then
-      echo "Copy apps/${app} to app-data/${app}..."
-      
-      # We typically want to stop and start during an app update
-      # However here we don't want this. Apps need to be updated avoiding 
-      # the 'stop' to avoid missing env errors
-      # And we can skip start as later in the update process, this happens automatically
-      "${UMBREL_ROOT}/scripts/app" update "${app}" --skip-stop --skip-start
-    fi
-  done
+  # Check whether repo was actually cloned
+  # By checking that is a README.md in the local app repo
+  LOCAL_REPO_PATH=$($UMBREL_ROOT/scripts/repo path)
+  if [[ -f "${LOCAL_REPO_PATH}/README.md" ]]; then
+    # 4. Update all apps using the app repo as source of truth
+    for app in $("${UMBREL_ROOT}/scripts/app" ls-installed); do
+      if [[ "${app}" != "" ]]; then
+        # We typically want to stop and start during an app update
+        # However here we don't want this. Apps need to be updated avoiding 
+        # the 'stop' to avoid missing env errors
+        # And we can skip start as later in the update process, this happens automatically
+        "${UMBREL_ROOT}/scripts/app" update "${app}" --skip-stop --skip-start
+      fi
+    done
 
-  # 5. Record that the migration is done
-  # So that it doesn't execute again
-  touch "${APP_REPOS_MIGRATION_DONE_FILE}"
-  
+    # 5. Record that the migration is done
+    # So that it doesn't execute again
+    touch "${APP_REPOS_MIGRATION_DONE_FILE}"
+
+    echo "Successfully migrated to enable app repos"
+  else
+    echo "Failed to clone remote app repo: ${DEFAULT_UMBREL_APP_REPO_URL}"
+  fi
+else
+  echo "Migration to app repos already completed"
 fi
