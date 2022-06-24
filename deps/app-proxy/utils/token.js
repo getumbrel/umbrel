441be8eed2919ccc1177523e0c899760135dab6a
Steven Briscoe <stevebriscoe123@gmail.com>
2022-06-24 17:01:22 +0100

    Show only first 12 characters of token in logs

--- deps/app-proxy/utils/token.js
+++ deps/app-proxy/utils/token.js
@@ -3,7 +3,7 @@ const manager = require('./manager.js');
 async function validate(token) {
 	if(typeof(token) !== "string") return false;
 
-	console.log(`Validating token: ${token}`);
+	console.log(`Validating token: ${token.substr(0, 12)} ...`);
 
 	try {
 		const info = await manager.account.token(token);
