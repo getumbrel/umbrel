7db3f023bcae91976133e16e3c2132789ef19b47
Luke Childs <lukechilds123@gmail.com>
2023-06-14 20:05:13 +0700

    Migrate Umbrel Home installs to using dhclient for DHCP

--- server/update/index.js
+++ server/update/index.js
@@ -1,4 +1,29 @@
 import fse from 'fs-extra'
+import {$} from 'execa'
+
+import isUmbrelHome from '../utilities/is-umbrel-home.js'
+
+async function updateNetworkManager() {
+  console.log('Patching network-manager to use dhclient...')
+
+  const configPath = '/etc/NetworkManager/NetworkManager.conf'
+  const configContents = `[main]
+plugins=ifupdown,keyfile
+dhcp=dhclient
+
+[ifupdown]
+managed=false
+`
+  if (!await fse.pathExists(configPath)) {
+    return console.log(`Skipping because config file doesn't exist`)
+  }
+  if((await fse.readFile(configPath, 'utf8')).includes('dhcp=dhclient')) {
+    return console.log(`Skipping because config file is already using dhclient`)
+  }
+  await fse.writeFile(configPath, configContents)
+  console.log('Restarting network-manager...')
+  await $`systemctl restart NetworkManager`
+}
 
 export default async function update({updateRoot, umbrelRoot}) {
   console.log(`Running migrations from "${updateRoot}" on "${umbrelRoot}"`)
@@ -16,5 +41,12 @@ export default async function update({updateRoot, umbrelRoot}) {
     console.log(`Updating "${umbrelPath}"...`)
 
     await fse.copy(updatePath, umbrelPath)
+  }
+
+  // Umbrel Home specific updates
+  if (await isUmbrelHome()) {
+    console.log('Running Umbrel Home specific migrations...')
+
+    await updateNetworkManager()
   }
 }
