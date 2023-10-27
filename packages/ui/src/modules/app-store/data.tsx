7acf6630688a71ca3b54941f6040a60db9a8b076
Mark Miro <contact@markmiro.com>
2023-10-27 10:48:56 -0700

    Make category descriptions same as on umbrel website

--- packages/ui/src/modules/app-store/data.tsx
+++ packages/ui/src/modules/app-store/data.tsx
@@ -4,18 +4,20 @@ import {Category} from '@/trpc/trpc'
 
 export type Categoryish = Category | 'all' | 'discover'
 
+// Same order as in this app store
+// https://apps.umbrel.com/category/developer
 export const categoryDescriptions = [
 	{id: 'discover', label: 'Discover'},
-	{id: 'all', label: 'All'},
-	{id: 'files', label: 'Files'},
+	{id: 'all', label: 'All apps'},
+	{id: 'files', label: 'Files & productivity'},
 	{id: 'bitcoin', label: 'Bitcoin'},
+	{id: 'finance', label: 'Finance'},
 	{id: 'media', label: 'Media'},
 	{id: 'networking', label: 'Networking'},
 	{id: 'social', label: 'Social'},
-	{id: 'automation', label: 'Automation'},
-	{id: 'finance', label: 'Finance'},
+	{id: 'automation', label: 'Home & automation'},
 	{id: 'ai', label: 'AI'},
-	{id: 'developer', label: 'Developer'},
+	{id: 'developer', label: 'Developer tools'},
 ] as const satisfies readonly {id: Categoryish; label: string}[]
 
 type CategoryDescription = (typeof categoryDescriptions)[number]
