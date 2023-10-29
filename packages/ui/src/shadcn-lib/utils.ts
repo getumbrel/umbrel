0e56aa55985d3b79cbfe146d1902be67f142661c
Mark Miro <contact@markmiro.com>
2023-10-28 19:41:13 -0700

    Fix issue `rounded` styles not overriding

--- packages/ui/src/shadcn-lib/utils.ts
+++ packages/ui/src/shadcn-lib/utils.ts
@@ -7,6 +7,8 @@ const customTwMerge = extendTailwindMerge({
 	classGroups: {
 		// Without this, styles like text-12 don't work properly with other text-* styles
 		'font-size': [{text: ['base', num]}],
+		// Allows cn('rounded-12', 'rounded-20') to cause the 20 to override the 12
+		'border-radius': [{rounded: ['base', num]}],
 		'border-width': [{border: ['hpx']}],
 	},
 })
