7a731c441295dc25235688d4a4015833915e0cd4
Mark Miro <contact@markmiro.com>
2023-11-02 17:11:28 -0700

    Animate in gallery section

--- packages/ui/src/modules/app-store/gallery-section.tsx
+++ packages/ui/src/modules/app-store/gallery-section.tsx
@@ -7,13 +7,14 @@ import {tw} from '@/utils/tw'
 export const AppsGallerySection: React.FC<{banners: Banner[]}> = ({banners}) => {
 	return (
 		<div className={galleryRootClass}>
-			{banners.map((banner) => (
+			{banners.map((banner, i) => (
 				<Link
 					key={banner.id}
 					to={`/app-store/${banner.id}`}
 					className={cn(galleryItemClass, 'h-[316px] w-[712px]')}
 					style={{
 						backgroundImage: `url(${banner.image})`,
+						animationDelay: `${i * 0.1}s`,
 					}}
 				/>
 			))}
@@ -24,12 +25,13 @@ export const AppsGallerySection: React.FC<{banners: Banner[]}> = ({banners}) => 
 export const AppGallerySection: React.FC<{gallery: string[]}> = ({gallery}) => {
 	return (
 		<div className={galleryRootClass}>
-			{gallery.map((src) => (
+			{gallery.map((src, i) => (
 				<button
 					key={src}
 					className={cn(galleryItemClass, 'h-[292px] w-[468px]')}
 					style={{
 						backgroundImage: `url(${src})`,
+						animationDelay: `${i * 0.1}s`,
 					}}
 				/>
 			))}
@@ -39,4 +41,4 @@ export const AppGallerySection: React.FC<{gallery: string[]}> = ({gallery}) => {
 
 export const galleryRootClass = tw`umbrel-fade-scroller-x umbrel-hide-scrollbar flex gap-5 overflow-x-auto`
 
-export const galleryItemClass = tw`shrink-0 rounded-10 bg-white/10 bg-cover outline-none ring-inset focus-visible:ring-4 ring-white/80`
+export const galleryItemClass = tw`shrink-0 rounded-10 bg-white/10 bg-cover outline-none ring-inset focus-visible:ring-4 ring-white/80 animate-in fade-in fill-mode-both slide-in-from-right-10`
