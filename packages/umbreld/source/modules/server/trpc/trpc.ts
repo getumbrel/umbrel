54bedf2a32a9dbea30b5c3003b31b008eedb284e
Mark Miro <contact@markmiro.com>
2023-11-03 16:28:39 -0700

    Add backend trpc zod error formatter

--- packages/umbreld/source/modules/server/trpc/trpc.ts
+++ packages/umbreld/source/modules/server/trpc/trpc.ts
@@ -1,9 +1,22 @@
+import {ZodError} from 'zod'
 import {initTRPC} from '@trpc/server'
 
 import {type Context} from './context.js'
 import {isAuthenticated} from './is-authenticated.js'
 
-const t = initTRPC.context<Context>().create()
+export const t = initTRPC.context<Context>().create({
+	// https://trpc.io/docs/server/error-formatting#adding-custom-formatting
+	errorFormatter(opts) {
+		const {shape, error} = opts
+		return {
+			...shape,
+			data: {
+				...shape.data,
+				zodError: error.code === 'BAD_REQUEST' && error.cause instanceof ZodError ? error.cause.flatten() : null,
+			},
+		}
+	},
+})
 export const router = t.router
 export const publicProcedure = t.procedure
 export const privateProcedure = t.procedure.use(isAuthenticated)
