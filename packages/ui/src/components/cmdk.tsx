a76e63d8ab5938745dcd7015d1e88310b4bb28ab
Mark Miro <contact@markmiro.com>
2023-11-07 10:00:27 -0800

    Don't show list of apps by default in cmdk menu

--- packages/ui/src/components/cmdk.tsx
+++ packages/ui/src/components/cmdk.tsx
@@ -1,20 +1,13 @@
-import {useEffect, useState} from 'react'
+import {useCommandState} from 'cmdk'
+import {ComponentPropsWithoutRef, useEffect, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 import {systemAppsKeyed, useInstalledApps} from '@/hooks/use-installed-apps'
-import {
-	CommandDialog,
-	CommandEmpty,
-	CommandGroup,
-	CommandInput,
-	CommandItem,
-	CommandList,
-} from '@/shadcn-components/ui/command'
+import {CommandDialog, CommandEmpty, CommandInput, CommandItem, CommandList} from '@/shadcn-components/ui/command'
 import {Separator} from '@/shadcn-components/ui/separator'
 import {fixmeHandler} from '@/utils/misc'
 
-import {Loading} from './ui/loading'
-
+// FIXME: searching "but" then pressing backspace to and typing "bit" gives zero results but it should at least show "Bitcoin"
 export function CmdkMenu() {
 	const navigate = useNavigate()
 	const {open, setOpen} = useCmdkOpen()
@@ -28,59 +21,58 @@ export function CmdkMenu() {
 			<Separator />
 			<CommandList>
 				<CommandEmpty>No results found.</CommandEmpty>
-				<CommandGroup>
-					<CommandItem
-						icon={systemAppsKeyed['settings'].icon}
-						onSelect={() => {
-							navigate('/settings/restart')
-							setOpen(false)
-						}}
-					>
-						Restart Umbrel
-					</CommandItem>
-					<CommandItem
-						icon={systemAppsKeyed['app-store'].icon}
-						onSelect={() => {
-							navigate('/app-store?dialog=updates')
-							setOpen(false)
-						}}
-					>
-						Update all apps
-					</CommandItem>
-					<CommandItem
-						icon={systemAppsKeyed['settings'].icon}
-						onSelect={() => {
-							navigate('/settings')
-							setOpen(false)
-						}}
-					>
-						Change wallpaper
-					</CommandItem>
-					<CommandItem
-						icon={systemAppsKeyed['home'].icon}
-						onSelect={() => {
-							navigate('/edit-widgets')
-							setOpen(false)
-						}}
-					>
-						Add widgets
-					</CommandItem>
-				</CommandGroup>
-				<CommandGroup heading='Apps'>
-					{isLoading ? (
-						<Loading />
-					) : (
-						installedApps.map((app) => (
-							<CommandItem icon={app.icon} key={app.id} onSelect={fixmeHandler}>
-								{app.name}
-								{/* <span className='text-white/50'>Open app</span> */}
-							</CommandItem>
-						))
-					)}
-				</CommandGroup>
+				<CommandItem
+					icon={systemAppsKeyed['settings'].icon}
+					onSelect={() => {
+						navigate('/settings/restart')
+						setOpen(false)
+					}}
+				>
+					Restart Umbrel
+				</CommandItem>
+				<CommandItem
+					icon={systemAppsKeyed['app-store'].icon}
+					onSelect={() => {
+						navigate('/app-store?dialog=updates')
+						setOpen(false)
+					}}
+				>
+					Update all apps
+				</CommandItem>
+				<CommandItem
+					icon={systemAppsKeyed['settings'].icon}
+					onSelect={() => {
+						navigate('/settings')
+						setOpen(false)
+					}}
+				>
+					Change wallpaper
+				</CommandItem>
+				<CommandItem
+					icon={systemAppsKeyed['home'].icon}
+					onSelect={() => {
+						navigate('/edit-widgets')
+						setOpen(false)
+					}}
+				>
+					Add widgets
+				</CommandItem>
+				{installedApps.map((app) => (
+					<SubItem icon={app.icon} key={app.id} onSelect={fixmeHandler}>
+						{app.name}
+						{/* <span className='text-white/50'>Open app</span> */}
+					</SubItem>
+				))}
 			</CommandList>
 		</CommandDialog>
 	)
+}
+
+const SubItem = (props: ComponentPropsWithoutRef<typeof CommandItem>) => {
+	const search = useCommandState((state) => state.search)
+	if (!search) return null
+
+	return <CommandItem {...props} />
 }
 
 export function useCmdkOpen() {
