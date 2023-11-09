b27edb281a6199ef95cd9965c9abd087f155249a
Mark Miro <contact@markmiro.com>
2023-11-09 09:23:45 -0800

    Unextract LargeIconButton into migration assistant page

--- packages/ui/src/routes/settings/migration-assistant.tsx
+++ packages/ui/src/routes/settings/migration-assistant.tsx
@@ -1,5 +1,7 @@
 import {motion} from 'framer-motion'
-import {Children} from 'react'
+import {LucideIcon} from 'lucide-react'
+import {Children, ReactNode} from 'react'
+import type {IconType} from 'react-icons'
 import {RiAlertFill} from 'react-icons/ri'
 import {TbAlertTriangleFilled, TbArrowBadgeRight, TbLock, TbPower, TbUsb} from 'react-icons/tb'
 import {useNavigate} from 'react-router-dom'
@@ -11,10 +13,10 @@ import {
 	ImmersiveDialogSplit,
 	immersiveDialogTitleClass,
 } from '@/components/ui/immersive-dialog'
-import {LargeIconButton} from '@/components/ui/large-icon-button'
 import {useQueryParams} from '@/hooks/use-query-params'
 import {useUmbrelTitle} from '@/hooks/use-umbrel-title'
 import {Button} from '@/shadcn-components/ui/button'
+import {cn} from '@/shadcn-lib/utils'
 
 const title = 'Migration Assistant'
 
@@ -83,9 +85,9 @@ function MigrationAssistantPrep() {
 				</>
 			}
 		>
-			<LargeIconButton icon={TbPower}>Shut down Raspberry Pi Umbrel</LargeIconButton>
-			<LargeIconButton icon={TbUsb}>Connect its SSD to Umbrel Home via USB</LargeIconButton>
-			<LargeIconButton icon={TbArrowBadgeRight}>Once done, click ‘Continue’ below.</LargeIconButton>
+			<IconMessage icon={TbPower} title='Shut down Raspberry Pi Umbrel' />
+			<IconMessage icon={TbUsb} title='Connect its SSD to Umbrel Home via USB' />
+			<IconMessage icon={TbArrowBadgeRight} title='Once done, click ‘Continue’ below.' />
 		</MigrationBody>
 	)
 }
@@ -118,20 +120,18 @@ function MigrationAssistantErrors() {
 				</>
 			}
 		>
-			<LargeIconButton
+			<IconMessage
 				icon={TbAlertTriangleFilled}
 				iconClassName='text-[#FFC107] [&>*]:stroke-none'
+				title='Couldn’t detect an SSD'
 				description='Make sure you’ve connected Raspberry Pi Umbrel’s SSD to your Umbrel Home'
-			>
-				Couldn’t detect an SSD
-			</LargeIconButton>
-			<LargeIconButton
+			/>
+			<IconMessage
 				icon={TbAlertTriangleFilled}
 				iconClassName='text-[#FFC107] [&>*]:stroke-none'
+				title='Incorrect SSD'
 				description='Make sure you’ve connected Raspberry Pi Umbrel’s SSD to your Umbrel Home'
-			>
-				Incorrect SSD
-			</LargeIconButton>
+			/>
 		</MigrationBody>
 	)
 }
@@ -152,20 +152,16 @@ function MigrationAssistantReady() {
 				</>
 			}
 		>
-			<LargeIconButton
+			<IconMessage
 				icon={TbLock}
+				title='Use the same password'
 				description='Remember to use Raspberry Pi Umbrel’s password to unlock your Umbrel Home'
-				className='pointer-events-none cursor-default'
-			>
-				Use the same password
-			</LargeIconButton>
-			<LargeIconButton
+			/>
+			<IconMessage
 				icon={TbPower}
+				title='Keep your Raspberry Pi off after the update'
 				description='This helps prevent issues with apps such as Lightning Node'
-				className='pointer-events-none cursor-default'
-			>
-				Keep your Raspberry Pi off after the update
-			</LargeIconButton>
+			/>
 		</MigrationBody>
 	)
 }
@@ -205,6 +201,44 @@ function MigrationBody({
 			</motion.div>
 			<div className='flex-1' />
 			<div className='flex flex-wrap-reverse items-center gap-2'>{footer}</div>
+		</div>
+	)
+}
+
+export function IconMessage({
+	icon,
+	title,
+	description,
+	className,
+	iconClassName,
+}: {
+	icon: IconType | LucideIcon
+	title: ReactNode
+	description?: ReactNode
+	className?: string
+	iconClassName?: string
+}) {
+	const IconComponent = icon
+
+	return (
+		<div
+			className={cn(
+				'inline-flex w-full items-center gap-2 rounded-10 border border-white/4 bg-white/4 p-2 text-left font-normal',
+				className,
+			)}
+			style={{
+				boxShadow: '0px 40px 60px 0px rgba(0, 0, 0, 0.10)',
+			}}
+		>
+			<div className='flex h-8 w-8 shrink-0 items-center justify-center rounded-8 bg-white/4'>
+				<IconComponent className={cn('h-5 w-5 [&>*]:stroke-1', iconClassName)} />
+			</div>
+			<div className='space-y-1'>
+				<div className='text-13 font-normal leading-tight -tracking-2'>{title}</div>
+				{description && (
+					<div className='text-12 font-normal leading-tight -tracking-2 text-white/50'>{description}</div>
+				)}
+			</div>
 		</div>
 	)
 }
