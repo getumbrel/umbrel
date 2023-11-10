c273eb8efa5933090a8d5be0e87305d8999d661c
Mark Miro <contact@markmiro.com>
2023-11-10 13:51:21 -0800

    Add title to category page

--- packages/ui/src/routes/app-store/category-page.tsx
+++ packages/ui/src/routes/app-store/category-page.tsx
@@ -1,6 +1,7 @@
 import {useParams} from 'react-router-dom'
 
 import {useAvailableApps} from '@/hooks/use-available-apps'
+import {useUmbrelTitle} from '@/hooks/use-umbrel-title'
 import {AppStoreNav} from '@/modules/app-store/app-store-nav'
 import {categoryDescriptionsKeyed, Categoryish} from '@/modules/app-store/constants'
 import {AppsGridFaintSection} from '@/modules/app-store/discover/apps-grid-section'
@@ -17,6 +18,8 @@ export function CategoryPage() {
 function CategoryContent() {
 	const {categoryishId} = useParams<{categoryishId: Categoryish}>()
 	const {appsGroupedByCategory, apps, isLoading} = useAvailableApps()
+
+	useUmbrelTitle(categoryishId ? categoryDescriptionsKeyed[categoryishId].label : '')
 
 	// Probably invalid url param
 	if (!categoryishId) return null
