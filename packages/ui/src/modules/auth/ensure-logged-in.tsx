2318a39f58dfb8a8a447c9e20913ee1f685f3679
Mark Miro <contact@markmiro.com>
2023-11-10 13:53:19 -0800

    Fix `EnsureLoggedIn` redirect because of new `isLoggedIn` trpc query

--- packages/ui/src/modules/auth/ensure-logged-in.tsx
+++ packages/ui/src/modules/auth/ensure-logged-in.tsx
@@ -9,25 +9,31 @@ export function EnsureLoggedIn({children}: {children?: React.ReactNode}) {
 	const location = useLocation()
 	const navigate = useNavigate()
 
-	const getQuery = trpcReact.user.isLoggedIn.useQuery(undefined, {
+	const {
+		data: isLoggedIn,
+		isLoading,
+		isError,
+	} = trpcReact.user.isLoggedIn.useQuery(undefined, {
 		retry: false,
 	})
 
-	if (getQuery.isLoading) {
+	if (isLoading) {
 		return <CoverMessage>Checking backend for user...</CoverMessage>
 	}
 
-	if (getQuery.error) {
-		if (!location.pathname.startsWith('/login')) {
-			sleep(500).then(() => navigate('/login'))
-			return <CoverMessage>Redirecting to login...</CoverMessage>
-		}
+	if (isError) {
+		return <CoverMessage>Error checking authentication status.</CoverMessage>
 	}
 
-	if (!getQuery?.data) {
+	if (isLoggedIn) {
 		if (location.pathname.startsWith('/login')) {
 			sleep(500).then(() => navigate('/'))
 			return <CoverMessage>Redirecting to home...</CoverMessage>
+		}
+	} else {
+		if (!location.pathname.startsWith('/login')) {
+			sleep(500).then(() => navigate('/login'))
+			return <CoverMessage delayed>Redirecting to login...</CoverMessage>
 		}
 	}
 
