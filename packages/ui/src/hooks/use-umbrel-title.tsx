de1ef12209318da5b76ef5f13f8ce44a1380e669
Mark Miro <contact@markmiro.com>
2023-11-10 13:46:02 -0800

    Add comment

--- packages/ui/src/hooks/use-umbrel-title.tsx
+++ packages/ui/src/hooks/use-umbrel-title.tsx
@@ -2,7 +2,9 @@ import {useEffect} from 'react'
 
 export function useUmbrelTitle(title: string) {
 	useEffect(() => {
+		// Allow hook to do nothing if no title provided
 		if (!title) return
+
 		const prevTitle = document.title
 		document.title = `${title} – Umbrel`
 		return () => {
