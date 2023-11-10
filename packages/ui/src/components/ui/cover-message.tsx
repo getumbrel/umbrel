19ccb4546500ddd85943c28843809c12e0acf928
Mark Miro <contact@markmiro.com>
2023-11-10 13:49:21 -0800

    Allow cover message to take delay

--- packages/ui/src/components/ui/cover-message.tsx
+++ packages/ui/src/components/ui/cover-message.tsx
@@ -1,6 +1,12 @@
+import {useTimeout} from 'react-use'
+
 /** Covers entire screen to show a message */
-export function CoverMessage({children}: {children: React.ReactNode}) {
+export function CoverMessage({children, delayed}: {children: React.ReactNode; delayed?: boolean}) {
+	const [show] = useTimeout(600)
+
 	return (
-		<div className='fixed inset-0 z-50 flex flex-col items-center justify-center gap-1 bg-black/80'>{children}</div>
+		<div className='fixed inset-0 z-50 flex flex-col items-center justify-center gap-1 bg-black/80'>
+			{!delayed ? children : show() && children}
+		</div>
 	)
 }
