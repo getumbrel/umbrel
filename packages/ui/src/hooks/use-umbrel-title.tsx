135f7c5bba2b58b258b765697aceb6f7a7faea8a
Mark Miro <contact@markmiro.com>
2023-11-10 13:44:36 -0800

    Allow useUmbrelTitle hook to do nothing if no title provided

--- packages/ui/src/hooks/use-umbrel-title.tsx
+++ packages/ui/src/hooks/use-umbrel-title.tsx
@@ -2,6 +2,7 @@ import {useEffect} from 'react'
 
 export function useUmbrelTitle(title: string) {
 	useEffect(() => {
+		if (!title) return
 		const prevTitle = document.title
 		document.title = `${title} – Umbrel`
 		return () => {
