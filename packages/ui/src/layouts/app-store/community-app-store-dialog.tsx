737cf739348c04159b96bc6e2a07e5cc022d0f77
Mark Miro <contact@markmiro.com>
2023-11-11 12:02:38 -0800

    trpc: support adding a community app store

--- packages/ui/src/layouts/app-store/community-app-store-dialog.tsx
+++ packages/ui/src/layouts/app-store/community-app-store-dialog.tsx
@@ -1,4 +1,6 @@
 import {DialogDescription} from '@radix-ui/react-dialog'
+import {useState} from 'react'
+import {toast} from 'sonner'
 
 import {DialogMounter} from '@/components/dialog-mounter'
 import {useQueryParams} from '@/hooks/use-query-params'
@@ -12,13 +14,43 @@ import {
 	DialogPortal,
 	DialogTitle,
 } from '@/shadcn-components/ui/dialog'
-import {Input} from '@/shadcn-components/ui/input'
+import {AnimatedInputError, Input} from '@/shadcn-components/ui/input'
+import {trpcReact} from '@/trpc/trpc'
 
 export function CommunityAppStoreDialog() {
 	const title = 'Add community store'
 	useUmbrelTitle(title)
-
 	const {params, removeParam} = useQueryParams()
+
+	// state
+
+	const [url, setUrl] = useState('')
+	const [localError, setLocalError] = useState('')
+
+	// mutations
+
+	const addAppStoreMut = trpcReact.appStore.addRepository.useMutation({
+		onSuccess: () => {
+			toast.success('Added community app store')
+			removeParam('dialog')
+		},
+	})
+
+	// handlers
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		addAppStoreMut.reset()
+		setLocalError('')
+		e.preventDefault()
+		if (!url) {
+			setLocalError('URL is required')
+			return
+		}
+		addAppStoreMut.mutate({url})
+	}
+
+	const remoteFormError = !addAppStoreMut.error?.data?.zodError && addAppStoreMut.error?.message
+	const formError = localError || remoteFormError
 
 	return (
 		<DialogMounter>
@@ -49,12 +81,24 @@ export function CommunityAppStoreDialog() {
 								the official Umbrel App Store team, and can potentially be insecure or malicious. Use caution and only
 								add app stores from developers you trust.
 							</p>
-							<Input placeholder='URL' />
-							<DialogFooter>
-								<Button variant='primary' size='dialog'>
-									Add
-								</Button>
-							</DialogFooter>
+							<form onSubmit={handleSubmit}>
+								<fieldset disabled={addAppStoreMut.isLoading} className='flex flex-col gap-5'>
+									<Input
+										placeholder='URL'
+										value={url}
+										onValueChange={setUrl}
+										variant={formError ? 'destructive' : 'default'}
+									/>
+									<div className='-my-2.5'>
+										<AnimatedInputError>{formError}</AnimatedInputError>
+									</div>
+									<DialogFooter>
+										<Button variant='primary' size='dialog'>
+											Add
+										</Button>
+									</DialogFooter>
+								</fieldset>
+							</form>
 						</div>
 					</DialogContent>
 				</DialogPortal>
