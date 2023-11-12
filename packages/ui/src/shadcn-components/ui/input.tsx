fedc9039af30afd634c495bf8abfb9e6e91e84df
Mark Miro <contact@markmiro.com>
2023-11-12 11:42:33 -0800

    Tweak input border on focus

--- packages/ui/src/shadcn-components/ui/input.tsx
+++ packages/ui/src/shadcn-components/ui/input.tsx
@@ -8,7 +8,7 @@ import {cn} from '@/shadcn-lib/utils'
 import {tw} from '@/utils/tw'
 
 const inputVariants = cva(
-	'flex h-12 w-full rounded-full border-hpx border-white/10 bg-white/4 hover:bg-white/6 px-5 py-2 text-15 font-medium -tracking-1 transition-colors duration-300 placeholder:text-white/30 focus-visible:placeholder:text-white/40 text-white/40 focus-visible:text-white focus-visible:bg-white/10 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-40',
+	'flex h-12 w-full rounded-full border-hpx border-white/10 bg-white/4 hover:bg-white/6 px-5 py-2 text-15 font-medium -tracking-1 transition-colors duration-300 placeholder:text-white/30 focus-visible:placeholder:text-white/40 text-white/40 focus-visible:text-white focus-visible:bg-white/10 focus-visible:outline-none focus-visible:border-white/50 disabled:cursor-not-allowed disabled:opacity-40',
 	{
 		variants: {
 			variant: {
