490ed9b9345cf4e462a76233b1ebac30f1e6a428
Mark Miro <contact@markmiro.com>
2023-11-16 12:06:33 -0800

    Create manual chunks to not put eveything in main bundle

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -10,4 +10,25 @@ export default defineConfig({
 			'@/': `${path.resolve(__dirname, 'src')}/`,
 		},
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				minifyInternalExports: true,
+				manualChunks: {
+					remeda: ['remeda'],
+					motion: ['framer-motion'],
+					bignumber: ['bignumber.js'],
+					i18n: ['i18next', 'react-i18next', 'i18next-browser-languagedetector', 'i18next-http-backend'],
+					fetch: ['@tanstack/react-query', '@trpc/react-query', '@trpc/client'],
+					css: ['tailwind-merge', 'clsx'],
+					dev: ['@tanstack/react-query-devtools', 'react-json-tree'],
+					sorter: ['match-sorter'],
+					icons: ['react-icons', 'lucide-react'],
+					qr: ['react-qr-code'],
+					pin: ['rci'],
+					colorThief: ['colorthief'],
+				},
+			},
+		},
+	},
 })
