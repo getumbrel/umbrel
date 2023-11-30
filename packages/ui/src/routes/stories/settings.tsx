cb0474894e8e863800235bf5c17437c140012db2
Mark Miro <contact@markmiro.com>
2023-11-30 14:13:02 -0800

    Update temp card styling for unknown and misc states

--- packages/ui/src/routes/stories/settings.tsx
+++ packages/ui/src/routes/stories/settings.tsx
@@ -1,6 +1,7 @@
 import {range} from 'remeda'
 
 import {Card} from '@/components/ui/card'
+import {H3} from '@/layouts/stories'
 import {Separator} from '@/shadcn-components/ui/separator'
 
 import {TempStatCardContent} from '../settings/_components/temp-stat-card-content'
@@ -8,12 +9,15 @@ import {TempStatCardContent} from '../settings/_components/temp-stat-card-conten
 export default function SettingsStory() {
 	return (
 		<div className='flex flex-col flex-wrap items-start gap-8 bg-white/10 p-8'>
+			<H3>undefined</H3>
 			<Card>
 				<TempStatCardContent />
 			</Card>
+			<H3>NaN</H3>
 			<Card>
 				<TempStatCardContent tempInCelcius={NaN} defaultUnit='c' />
 			</Card>
+			<H3>Infinity</H3>
 			<Card>
 				<TempStatCardContent tempInCelcius={Infinity} defaultUnit='c' />
 			</Card>
