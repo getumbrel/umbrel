77a9f8034f28131f05863ea11bc775c42cc08f0a
Mark Miro <contact@markmiro.com>
2023-12-07 15:16:42 -0800

    Add osVersion query to backend

--- packages/umbreld/source/modules/server/trpc/routes/system.ts
+++ packages/umbreld/source/modules/server/trpc/routes/system.ts
@@ -1,8 +1,11 @@
-import {router, privateProcedure} from '../trpc.js'
+import {router, privateProcedure, publicProcedure} from '../trpc.js'
 
 import {getCpuTemperature, getDiskUsage, getMemoryUsage, shutdown, reboot} from '../../../system.js'
 
 export default router({
+	// Public because we might want to ask user to update before they even log in?
+	osVersion: publicProcedure.query(() => '1.0.0'),
+	//
 	cpuTemperature: privateProcedure.query(() => getCpuTemperature()),
 	diskUsage: privateProcedure.query(({ctx}) => getDiskUsage(ctx.umbreld.dataDirectory)),
 	memoryUsage: privateProcedure.query(() => getMemoryUsage()),
