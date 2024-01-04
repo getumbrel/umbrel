cff609f055426e2671b8b5e8d46635c9773cc262
Mark Miro <contact@markmiro.com>
2024-01-04 12:24:48 -0800

    Remove toast on doing a ⌘K action

--- packages/ui/src/utils/track-app-open.ts
+++ packages/ui/src/utils/track-app-open.ts
@@ -1,8 +1,5 @@
-import {toast} from 'sonner'
-
 import {trpcClient} from '@/trpc/trpc'
 
 export function trackAppOpen(appId: string) {
 	trpcClient.user.apps.trackAppOpen.mutate({appId})
-	toast(`${appId} opened`)
 }
