81bd24d38fad6e05713b415cd6f96e3e2f2d9e15
Mark Miro <contact@markmiro.com>
2024-01-10 09:22:49 -0800

    Tweak styles in mobile list row in settings to work better at small viewports

--- packages/ui/src/routes/settings/_components/list-row.tsx
+++ packages/ui/src/routes/settings/_components/list-row.tsx
@@ -49,16 +49,13 @@ export function ListRowMobile({
 	const Icon = icon
 
 	return (
-		<button
-			className={cn('flex w-full flex-wrap items-center gap-x-4 gap-y-2.5 px-2.5 py-3 text-left')}
-			onClick={onClick}
-		>
-			<div className='flex h-8 w-8 items-center justify-center rounded-6 bg-white/6'>
+		<button className={cn('flex w-full items-center gap-x-4 gap-y-2.5 px-2.5 py-3 text-left')} onClick={onClick}>
+			<div className='flex h-8 w-8 shrink-0 items-center justify-center rounded-6 bg-white/6'>
 				{Icon && <Icon className={cn('h-5 w-5 text-brand [&>*]:stroke-2')} />}
 			</div>
-			<div className='flex flex-col gap-1'>
+			<div className='flex min-w-0 flex-col gap-1'>
 				<h3 className='text-13 font-medium leading-none -tracking-2'>{title}</h3>
-				<p className='text-12 leading-none -tracking-2 text-white/40'>{description}</p>
+				<p className='truncate text-12 leading-none -tracking-2 text-white/40'>{description}</p>
 			</div>
 			{children}
 		</button>
