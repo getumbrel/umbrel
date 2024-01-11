5d9735d22f126ff99a1c0af91bd3947e0a13f930
Mark Miro <contact@markmiro.com>
2024-01-11 08:44:01 -0800

    Prevent drawer background scaling by default
    Doesn't do anything atm anyways

--- packages/ui/src/shadcn-components/ui/drawer.tsx
+++ packages/ui/src/shadcn-components/ui/drawer.tsx
@@ -3,7 +3,7 @@ import {Drawer as DrawerPrimitive} from 'vaul'
 
 import {cn} from '@/shadcn-lib/utils'
 
-const Drawer = ({shouldScaleBackground = true, ...props}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
+const Drawer = ({shouldScaleBackground = false, ...props}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
 	<DrawerPrimitive.Root shouldScaleBackground={shouldScaleBackground} {...props} />
 )
 Drawer.displayName = 'Drawer'
