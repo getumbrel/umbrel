6c5a5e2149eb2b274cea8879d1118fe9ad6bb1e2
Mark Miro <contact@markmiro.com>
2024-01-16 12:13:23 -0800

    Remove disabled cursor on some components

--- packages/ui/src/shadcn-components/ui/checkbox.tsx
+++ packages/ui/src/shadcn-components/ui/checkbox.tsx
@@ -12,7 +12,7 @@ const Checkbox = React.forwardRef<
 	<CheckboxPrimitive.Root
 		ref={ref}
 		className={cn(
-			'group peer h-5 w-5 shrink-0 rounded-5 border border-white/20 bg-white/10 ring-offset-neutral-950 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-brand data-[state=indeterminate]:bg-brand data-[state=checked]:text-neutral-50 data-[state=checked]:text-white data-[state=indeterminate]:text-neutral-50 data-[state=indeterminate]:text-white',
+			'group peer h-5 w-5 shrink-0 rounded-5 border border-white/20 bg-white/10 ring-offset-neutral-950 transition-[color,background-color,opacity] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-brand data-[state=indeterminate]:bg-brand data-[state=checked]:text-neutral-50 data-[state=checked]:text-white data-[state=indeterminate]:text-neutral-50 data-[state=indeterminate]:text-white',
 			className,
 		)}
 		{...props}
@@ -37,6 +37,7 @@ const Checkbox = React.forwardRef<
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 const checkboxContainerClass = tw`flex items-center space-x-2`
-const checkboxLabelClass = tw`select-none text-15 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-50`
+// Removing `peer-disabled:cursor-not-allowed` because we want to disable the checkbox while it's going to the server without changing the cursor
+const checkboxLabelClass = tw`select-none text-15 font-medium leading-none peer-disabled:opacity-50`
 
 export {Checkbox, checkboxContainerClass, checkboxLabelClass}
