a233bc1b0753625fdd0e8cabe4c81eb2f1d3e56e
Mark Miro <contact@markmiro.com>
2024-01-25 14:25:18 -0800

    Show app icon placeholder only when not loaded
    Fixes default showing behind loaded icon in cmdk menu

--- packages/ui/src/components/app-icon.tsx
+++ packages/ui/src/components/app-icon.tsx
@@ -1,18 +1,20 @@
-import {forwardRef, HTMLProps} from 'react'
+import {forwardRef, HTMLProps, useState} from 'react'
 
 import {cn} from '@/shadcn-lib/utils'
 
 type AppIconProps = {src: string; size?: number} & HTMLProps<HTMLImageElement>
 
 function ForwardedAppIcon({src, style, size, className, ...props}: AppIconProps, ref: React.Ref<HTMLImageElement>) {
+	const [loaded, setLoaded] = useState(false)
 	const defaultIcon = '/icons/app-icon-placeholder.svg'
 
 	return (
 		<img
-			src={src || defaultIcon}
+			src={src}
 			alt=''
 			ref={ref}
-			className={cn('aspect-square shrink-0 bg-white/10 bg-cover bg-center', className)}
+			className={cn('aspect-square shrink-0 bg-cover bg-center', !loaded && 'bg-white/10', className)}
+			onLoad={() => setLoaded(true)}
 			style={{
 				...style,
 				width: size,
@@ -20,7 +22,8 @@ function ForwardedAppIcon({src, style, size, className, ...props}: AppIconProps,
 				minWidth: size,
 				minHeight: size,
 				// borderRadius: (size * 15) / 50, // 15px for 50px size
-				backgroundImage: `url(${defaultIcon})`,
+				backgroundImage: !loaded ? `url(${defaultIcon})` : undefined,
+				backgroundColor: !loaded ? 'transparent' : undefined,
 			}}
 			{...props}
 		/>
