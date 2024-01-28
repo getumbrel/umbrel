e3ef788e9cecb6039f67e92dc24dc7d34125d7a9
Mark Miro <contact@markmiro.com>
2024-01-28 15:07:31 -0800

    Comment out

--- packages/ui/src/modules/migrate/migrate-image.tsx
+++ packages/ui/src/modules/migrate/migrate-image.tsx
@@ -1,5 +1,5 @@
 const FROM_RASPBERRY_PI_URL = '/figma-exports/migrate-raspberrypi-umbrel-home.png'
-const FROM_UMBREL_URL = '/figma-exports/migrate-umbrel-home-umbrel-home.png'
+// const FROM_UMBREL_URL = '/figma-exports/migrate-umbrel-home-umbrel-home.png'
 
 export function MigrateImage() {
 	// TODO: call API to determine if we're migrating from Umbrel or Raspberry Pi
