e6b60f3011fea68ac767e805ce2893a378efa604
Mark Miro <contact@markmiro.com>
2024-02-15 09:03:11 -0800

    Fix label function not being called

--- packages/ui/src/routes/app-store/category-page.tsx
+++ packages/ui/src/routes/app-store/category-page.tsx
@@ -19,7 +19,7 @@ function CategoryContent() {
 	const {categoryishId} = useParams<{categoryishId: Categoryish}>()
 	const {appsGroupedByCategory, apps, isLoading} = useAvailableApps()
 
-	useUmbrelTitle(categoryishId ? categoryDescriptionsKeyed[categoryishId].label : '')
+	useUmbrelTitle(categoryishId ? categoryDescriptionsKeyed[categoryishId].label() : '')
 
 	// Probably invalid url param
 	if (!categoryishId) return null
@@ -28,7 +28,7 @@ function CategoryContent() {
 	const categoryId = categoryishId === 'discover' || categoryishId === 'all' ? null : categoryishId
 
 	const filteredApps = categoryId ? appsGroupedByCategory[categoryId] : apps
-	const title = categoryDescriptionsKeyed[categoryishId].label
+	const title = categoryDescriptionsKeyed[categoryishId].label()
 
 	return <AppsGridFaintSection title={title} apps={filteredApps} />
 }
