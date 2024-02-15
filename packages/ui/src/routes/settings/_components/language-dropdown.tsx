e892b765cc945178814d6e2f655f306ea4dd6572
Mark Miro <contact@markmiro.com>
2024-02-15 10:31:56 -0800

    Simplify language hook logic

--- packages/ui/src/routes/settings/_components/language-dropdown.tsx
+++ packages/ui/src/routes/settings/_components/language-dropdown.tsx
@@ -11,35 +11,37 @@ import {
 } from '@/shadcn-components/ui/dropdown-menu'
 
 export function LanguageDropdown() {
+	return (
+		<DropdownMenu>
+			<LanguageDropdownTrigger />
+			<LanguageDropdownContent />
+		</DropdownMenu>
+	)
+}
+
+export function LanguageDropdownTrigger() {
+	const [activeCode] = useLanguage()
+
+	return (
+		<DropdownMenuTrigger asChild>
+			<IconButton icon={Globe}>
+				{languages.find(({code}) => code === activeCode)?.name}
+				<ChevronDown />
+			</IconButton>
+		</DropdownMenuTrigger>
+	)
+}
+
+export function LanguageDropdownContent() {
 	const [activeCode, setActiveCode] = useLanguage()
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger asChild>
-				<IconButton icon={Globe} id='language'>
-					{languages.find(({code}) => code === activeCode)?.name}
-					<ChevronDown />
-				</IconButton>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end'>
-				{languages.map(({code, name}) => (
-					<DropdownMenuCheckboxItem
-						key={code}
-						checked={activeCode === code}
-						onSelect={() => {
-							// Wait for the dropdown to close before changing the language
-							setTimeout(() => {
-								setActiveCode(code)
-								// Reload because I don't want to deal with the complexity of changing the language on the fly
-								// Keeping the state in sync with the language would be a pain
-								window.location.reload()
-							}, 200)
-						}}
-					>
-						{name}
-					</DropdownMenuCheckboxItem>
-				))}
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<DropdownMenuContent align='end'>
+			{languages.map(({code, name}) => (
+				<DropdownMenuCheckboxItem key={code} checked={activeCode === code} onSelect={() => setActiveCode(code)}>
+					{name}
+				</DropdownMenuCheckboxItem>
+			))}
+		</DropdownMenuContent>
 	)
 }
