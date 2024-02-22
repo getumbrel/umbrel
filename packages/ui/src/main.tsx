e9cf8b4742073cfd2193942beeedab49e4f8da82
Mark Miro <contact@markmiro.com>
2024-02-22 12:45:26 -0800

    Don't show page cover is backend unavailable

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -12,7 +12,6 @@ import {ErrorBoundary} from 'react-error-boundary'
 import {IframeChecker} from './components/iframe-checker'
 import {BareCoverMessage} from './components/ui/cover-message'
 import {Toaster} from './components/ui/toast'
-import {EnsureBackendAvailable} from './modules/auth/ensure-backend-available'
 import {RemoteWallpaperInjector, WallpaperProvider} from './providers/wallpaper'
 import {router} from './router'
 import {TooltipProvider} from './shadcn-components/ui/tooltip'
@@ -25,15 +24,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 			{/* <UpdatingCover> */}
 			<ErrorBoundary fallback={<BareCoverMessage>{t('something-went-wrong')}</BareCoverMessage>}>
 				<TrpcProvider>
-					<EnsureBackendAvailable>
-						<WallpaperProvider>
-							<RemoteWallpaperInjector />
-							<TooltipProvider>
-								{/* TODO: move stories out of main router */}
-								<RouterProvider router={router} />
-							</TooltipProvider>
-						</WallpaperProvider>
-					</EnsureBackendAvailable>
+					<WallpaperProvider>
+						<RemoteWallpaperInjector />
+						<TooltipProvider>
+							{/* TODO: move stories out of main router */}
+							<RouterProvider router={router} />
+						</TooltipProvider>
+					</WallpaperProvider>
 				</TrpcProvider>
 				<Toaster />
 			</ErrorBoundary>
