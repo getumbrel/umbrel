472491dab0e3bb96aa0b6a2f36566c0006456d54
nmfretz <nmfretz@gmail.com>
2024-02-28 10:41:53 -0800

    break out system-widget functions to system-widgets.ts

--- packages/umbreld/source/modules/server/trpc/routes/widget.ts
+++ packages/umbreld/source/modules/server/trpc/routes/widget.ts
@@ -1,93 +1,12 @@
 import z from 'zod'
 import {$} from 'execa'
 import fetch from 'node-fetch'
-import prettyBytes from 'pretty-bytes'
 import ms from 'ms'
 
 import {router, privateProcedure} from '../trpc.js'
-import type Umbreld from '../../../../index.js'
-import { getDiskUsage, getMemoryUsage, getCpuUsage } from '../../../system.js'
+import {systemWidgets} from '../../../system-widgets.js'
 
 const MAX_ALLOWED_WIDGETS = 3
-
-async function getStorageData(umbreld: Umbreld) {
-	const {size, totalUsed} = await getDiskUsage(umbreld)
-
-	return {
-		type: 'stat-with-progress',
-		link: '?dialog=live-usage',
-		refresh: '30s',
-		title: 'Storage',
-		value: prettyBytes(totalUsed),
-		progressLabel: `${prettyBytes(size - totalUsed)} left`,
-		progress: ( totalUsed / size ).toFixed(2),
-	}
-}
-
-async function getMemoryData(umbreld: Umbreld) {
-	const {size, totalUsed} = await getMemoryUsage(umbreld)
-
-	return {
-		type: 'stat-with-progress',
-		link: '?dialog=live-usage',
-		refresh: '10s',
-		title: 'Memory',
-		value: prettyBytes(totalUsed),
-		valueSub: prettyBytes(size),
-		progressLabel: `${prettyBytes(size - totalUsed)} left`,
-		progress: ( totalUsed / size ).toFixed(2),
-	}
-}
-
-async function getSystemStats(umbreld: Umbreld) {
-	const [cpuUsage, diskUsage, memoryUsage] = await Promise.all([
-		getCpuUsage(umbreld),
-		getDiskUsage(umbreld),
-		getMemoryUsage(umbreld)
-	]);
-
-	const { totalUsed: cpuTotalUsed } = cpuUsage;
-	const {size: diskSize, totalUsed: diskTotalUsed} = diskUsage;
-	const {size: memorySize, totalUsed: memoryTotalUsed} = memoryUsage;
-
-	return {
-		type: 'three-up',
-		link: '?dialog=live-usage',
-		refresh: '10s',
-		items: [
-			{
-				icon: 'system-widget-cpu',
-				title: 'CPU',
-				value: `${cpuTotalUsed.toPrecision(2)}%`,
-			},
-			{
-				icon: 'system-widget-storage',
-				title: 'Free',
-				value: `${prettyBytes(diskSize - diskTotalUsed)}`,
-			},
-			{
-				icon: 'system-widget-memory',
-				title: 'Memory',
-				value: `${prettyBytes(memorySize - memoryTotalUsed)}`,
-			}
-		]
-	}
-}
-
-const systemWidgets = {
-  storage: async function(umbreld: Umbreld) {
-    const data = await getStorageData(umbreld)
-    return data
-  },
-  memory: async function(umbreld: Umbreld) {
-    const data = await getMemoryData(umbreld)
-    return data
-  },
-	'system-stats': async function(umbreld: Umbreld) {
-		const data = await getSystemStats(umbreld)
-		return data
-	}
-}
 
 // Splits a widgetId into appId and widgetName
 // e.g., "transmission:status" => { appId: "transmission", widgetName: "status" }
