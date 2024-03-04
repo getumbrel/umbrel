bdbefe8092aa40e4eb2dd1d8dac528918cb12193
Mark Miro <contact@markmiro.com>
2024-03-04 09:16:03 -0800

    Remove auto-update and notifications from app-store

--- packages/ui/src/modules/app-store/app-page/settings-section.tsx
+++ packages/ui/src/modules/app-store/app-page/settings-section.tsx
@@ -1,9 +1,7 @@
 import {ReactNode} from 'react'
-import {toast} from 'sonner'
 
 import {CopyableField} from '@/components/ui/copyable-field'
 import {UNKNOWN} from '@/constants'
-import {Switch} from '@/shadcn-components/ui/switch'
 import {trpcReact, UserApp} from '@/trpc/trpc'
 import {t} from '@/utils/i18n'
 
@@ -12,55 +10,9 @@ import {cardClass, cardTitleClass} from './shared'
 export function SettingsSection({userApp}: {userApp: UserApp}) {
 	const ctx = trpcReact.useContext()
 
-	// @ts-expect-error `autoUpdate`
-	const autoUpdateMut = trpcReact.apps.autoUpdate.useMutation({
-		onSuccess: () => ctx.apps.invalidate(),
-	})
-
-	// @ts-expect-error `showNotifications`
-	const showNotificationsMut = trpcReact.apps.showNotifications.useMutation({
-		onSuccess: () => ctx.apps.invalidate(),
-	})
-
-	const handleAutoUpdateChange = (checked: boolean) => {
-		// TODO: use mutation
-		toast(`Auto-update to ${checked} setting is not available yet`)
-		autoUpdateMut.mutate({appId: userApp.id, autoUpdate: checked})
-	}
-
-	const handleShowNotifcationsChange = (checked: boolean) => {
-		// TODO: use mutation
-		toast(`Notifications setting to ${checked} is not available yet`)
-		showNotificationsMut.mutate({appId: userApp.id, showNotifications: checked})
-	}
-
 	return (
 		<div className={cardClass}>
 			<h2 className={cardTitleClass}>{t('app-page.section.settings.title')}</h2>
-			<label>
-				<KV
-					k={t('auto-update')}
-					v={
-						<Switch
-							checked={userApp.autoUpdate}
-							onCheckedChange={handleAutoUpdateChange}
-							disabled={autoUpdateMut.isLoading}
-						/>
-					}
-				/>
-			</label>
-			<label>
-				<KV
-					k={t('notifications')}
-					v={
-						<Switch
-							checked={userApp.showNotifications}
-							onCheckedChange={handleShowNotifcationsChange}
-							disabled={showNotificationsMut.isLoading}
-						/>
-					}
-				/>
-			</label>
 			<KV
 				k={t('default-credentials.username')}
 				v={<CopyableField className='w-[120px]' narrow value={userApp.credentials.defaultUsername} />}
