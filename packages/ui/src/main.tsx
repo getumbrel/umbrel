0e79877d55db4ccaca16b7197fc0447bff2f7289
Mark Miro <contact@markmiro.com>
2024-03-05 13:27:44 -0800

    Extract `init` function to keep `app-auth` in sync with main app

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,51 +1,19 @@
-import 'inter-ui/inter.css'
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
 
-import './index.css'
-import './utils/i18n'
-
-import i18next from 'i18next'
-import {ErrorBoundary} from 'react-error-boundary'
-import {HelmetProvider} from 'react-helmet-async'
-
+import {init} from '@/init'
 import {initTokenRenewal} from '@/modules/auth/shared'
 
-import {IframeChecker} from './components/iframe-checker'
-import {BareCoverMessage, CoverMessageTarget} from './components/ui/cover-message'
-import {Toaster} from './components/ui/toast'
 import {RemoteWallpaperInjector, WallpaperProvider} from './providers/wallpaper'
 import {router} from './router'
-import {TooltipProvider} from './shadcn-components/ui/tooltip'
 import {TrpcProvider} from './trpc/trpc-provider'
-import {t} from './utils/i18n'
 
 initTokenRenewal()
 
-i18next.on('initialized', () => {
-	ReactDOM.createRoot(document.getElementById('root')!).render(
-		<React.StrictMode>
-			<HelmetProvider>
-				<IframeChecker>
-					{/* <UpdatingCover> */}
-					<ErrorBoundary fallback={<BareCoverMessage>{t('something-went-wrong')}</BareCoverMessage>}>
-						<TrpcProvider>
-							<WallpaperProvider>
-								<RemoteWallpaperInjector />
-								<TooltipProvider>
-									{/* TODO: move stories out of main router */}
-									<RouterProvider router={router} />
-								</TooltipProvider>
-							</WallpaperProvider>
-						</TrpcProvider>
-						<Toaster />
-						{/* Want to show cover message over Toast elements */}
-						<CoverMessageTarget />
-					</ErrorBoundary>
-				</IframeChecker>
-			</HelmetProvider>
-		</React.StrictMode>,
-	)
-})
+init(
+	<TrpcProvider>
+		<WallpaperProvider>
+			<RemoteWallpaperInjector />
+			<RouterProvider router={router} />
+		</WallpaperProvider>
+	</TrpcProvider>,
+)
