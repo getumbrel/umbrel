32f3524f62304cd609fd7737ef1029cfa640e87f
Mark Miro <contact@markmiro.com>
2024-03-05 13:42:47 -0800

    Fix agressive CPU temp updating when the first call failed
    Prevents console from being spammed

--- packages/ui/src/hooks/use-cpu-temp.ts
+++ packages/ui/src/hooks/use-cpu-temp.ts
@@ -1,13 +1,21 @@
+import {useState} from 'react'
+
 import {trpcReact} from '@/trpc/trpc'
 import {isCpuTooCold, isCpuTooHot} from '@/utils/system'
 import {CpuType} from '@/utils/tempurature'
 
 export function useCpuTemp() {
+	const [enabled, setEnabled] = useState(true)
 	const cpuTempQ = trpcReact.system.cpuTemperature.useQuery(undefined, {
 		// Sometimes we won't be able to get CPU temp, so prevent retry
 		retry: false,
 		// We do want refetching to happen on a schedule though
 		refetchInterval: 5_000,
+		enabled,
+		onError: () => {
+			// Don't spam console with errors
+			setEnabled(false)
+		},
 	})
 
 	const temp = cpuTempQ.data
