df7294570453c08421f320563cba73710e00d587
Mark Miro <contact@markmiro.com>
2024-03-09 05:08:46 +0000

    Add wallpaper to cover message story

--- packages/ui/src/routes/stories/cover.tsx
+++ packages/ui/src/routes/stories/cover.tsx
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 
 import {BareCoverMessage, CoverMessage, CoverMessageParagraph} from '@/components/ui/cover-message'
+import {Wallpaper} from '@/providers/wallpaper'
 import {Button} from '@/shadcn-components/ui/button'
 
 const covers = ['none', 'bare', 'default']
@@ -11,23 +12,26 @@ export default function Cover() {
 	const [cover, setCover] = useState<Cover>('none')
 
 	return (
-		<div>
-			{covers.map((cover) => (
-				<Button size='lg' key={cover} onClick={() => setCover(cover)}>
-					{cover}
-				</Button>
-			))}
-			{cover === 'none' && <div>No cover</div>}
-			{cover === 'bare' && (
-				<BareCoverMessage onClick={() => setCover('none')}>
-					<CoverMessageParagraph>Bare</CoverMessageParagraph>
-				</BareCoverMessage>
-			)}
-			{cover === 'default' && (
-				<CoverMessage onClick={() => setCover('none')} bodyClassName='delay-300'>
-					<CoverMessageParagraph>Default</CoverMessageParagraph>
-				</CoverMessage>
-			)}
-		</div>
+		<>
+			<Wallpaper />
+			<div className='relative'>
+				{covers.map((cover) => (
+					<Button size='lg' key={cover} onClick={() => setCover(cover)}>
+						{cover}
+					</Button>
+				))}
+				{cover === 'none' && <div>No cover</div>}
+				{cover === 'bare' && (
+					<BareCoverMessage onClick={() => setCover('none')}>
+						<CoverMessageParagraph>Bare</CoverMessageParagraph>
+					</BareCoverMessage>
+				)}
+				{cover === 'default' && (
+					<CoverMessage onClick={() => setCover('none')} bodyClassName='delay-300'>
+						<CoverMessageParagraph>Default</CoverMessageParagraph>
+					</CoverMessage>
+				)}
+			</div>
+		</>
 	)
 }
