cae971396157ed9af8f08ccf20763fde6df7090e
Luke Childs <lukechilds123@gmail.com>
2024-03-10 16:40:38 +0700

    Fix logs

--- packages/umbreld/source/modules/apps/legacy-compat/app-script.ts
+++ packages/umbreld/source/modules/apps/legacy-compat/app-script.ts
@@ -5,13 +5,13 @@ import {$} from 'execa'
 
 import type Umbreld from '../../../index.js'
 
-export default async function appScript(umbreld: Umbreld, command: string, arg: string) {
+export default async function appScript(umbreld: Umbreld, command: string, arg: string, inheritStdio: boolean = true) {
 	const currentFilename = fileURLToPath(import.meta.url)
 	const currentDirname = dirname(currentFilename)
 	const scriptPath = join(currentDirname, 'app-script')
 	const SCRIPT_APP_REPO_DIR = await umbreld.appStore.getAppTemplateFilePath(arg)
 	return $({
-		stdio: 'inherit',
+		stdio: inheritStdio ? 'inherit' : 'pipe',
 		env: {
 			SCRIPT_UMBREL_ROOT: umbreld.dataDirectory,
 			SCRIPT_DOCKER_FRAGMENTS: currentDirname,
