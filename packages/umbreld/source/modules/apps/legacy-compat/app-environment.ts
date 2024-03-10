3865a28c4cf2e4bbf731b59cc90b5d49221e491c
Luke Childs <lukechilds123@gmail.com>
2024-03-10 16:41:31 +0700

    Fix tor-proxy container

--- packages/umbreld/source/modules/apps/legacy-compat/app-environment.ts
+++ packages/umbreld/source/modules/apps/legacy-compat/app-environment.ts
@@ -23,9 +23,12 @@ export default async function appEnvironment(umbreld: Umbreld, command: string) 
 			AUTH_PORT: '2000',
 			TOR_PROXY_IP: '10.21.21.11',
 			TOR_PROXY_PORT: '9050',
+			TOR_PASSWORD: 'mLcLDdt5qqMxlq3wv8Din3UD44bTZHzRFhIktw38kWg=',
+			TOR_HASHED_PASSWORD: '16:158FBE422B1A9D996073BE2B9EC38852C70CE12362CA016F8F6859C426',
 			UMBREL_AUTH_SECRET: 'DEADBEEF',
 			JWT_SECRET: await umbreld.server.getJwtSecret(),
 			UMBRELD_RPC_HOST: `host.docker.internal:${umbreld.server.port}`, // TODO: Check host.docker.internal works on linux
+			UMBREL_LEGACY_COMPAT_DIR: currentDirname,
 		},
 	}
 	if (command === 'up') {
