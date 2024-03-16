b840eaef5bc92a0f96326464815a43acfaa0b0e5
Mayank Chhabra <mayankchhabra9@gmail.com>
2024-03-17 04:37:30 +0800

    Use 'Umbrel' title on all pages

--- packages/ui/src/components/umbrel-head-title.tsx
+++ packages/ui/src/components/umbrel-head-title.tsx
@@ -1,12 +1,9 @@
 import {Helmet} from 'react-helmet-async'
 
-import {t} from '@/utils/i18n'
-
 export function UmbrelHeadTitle({children}: {children: string}) {
-	const title = children
 	return (
 		<Helmet>
-			<title>{t('page-title-umbrel', {title})}</title>
+			<title>Umbrel</title>
 		</Helmet>
 	)
 }
