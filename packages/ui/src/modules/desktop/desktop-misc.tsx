5dcfff0669f488a6e0005a511b3a7e8479b18803
Mayank Chhabra <mayankchhabra9@gmail.com>
2024-03-17 00:57:24 +0800

    Remove bg blur from cmd k search button

--- packages/ui/src/modules/desktop/desktop-misc.tsx
+++ packages/ui/src/modules/desktop/desktop-misc.tsx
@@ -23,7 +23,7 @@ export function Search({onClick}: {onClick?: () => void}) {
 	const isMobile = useIsSmallMobile()
 	return (
 		<button
-			className='z-10 select-none rounded-full bg-neutral-600/10 px-3 py-2.5 text-12 leading-inter-trimmed text-white/75 backdrop-blur-lg transition-colors delay-500 duration-300 animate-in fade-in fill-mode-both hover:bg-neutral-600/30 active:bg-neutral-600/10'
+			className='z-10 select-none rounded-full bg-neutral-600/20 px-3 py-2.5 text-12 leading-inter-trimmed text-white/90 transition-colors delay-500 duration-300 animate-in fade-in fill-mode-both hover:bg-neutral-600/30 active:bg-neutral-600/10'
 			onClick={onClick}
 		>
 			{/* TODO: ideally, centralize shortcut preview and shortcut event listener so always in sync */}
